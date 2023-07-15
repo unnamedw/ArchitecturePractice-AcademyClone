8f7202b470370ef9443f9fe0370e146e8592709a
doach <msg_billie@naver.com>
2023-07-15 18:32:50 +0900

    [#1] 설정 및 파일 위치 변경

--- app/src/main/java/com/doachgosum/eliceacademyclone/presentation/MainActivity.kt
+++ app/src/main/java/com/doachgosum/eliceacademyclone/presentation/MainActivity.kt
@@ -2,42 +2,11 @@ package com.doachgosum.eliceacademyclone.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
 import com.doachgosum.eliceacademyclone.R
-import com.doachgosum.eliceacademyclone.data.remote.request_param.FilterConditionRequestParam
-import com.doachgosum.eliceacademyclone.presentation.util.getAppContainer
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        lifecycleScope.launch(Dispatchers.IO) {
-            getAppContainer().courseApi.also {
-                it.getAllCourse(
-                    offset = 0,
-                    count = 10,
-                    filterIsRecommended = true,
-                    filterIsFree = false,
-                    paramJson = FilterConditionRequestParam(
-                        courseIds = listOf(1, 2, 3)
-                    )
-                )
-
-                it.getCourseDetail(courseId = 18817)
-            }
-
-            getAppContainer().lectureApi.also {
-                it.getLectureList(
-                    offset = 0,
-                    count = 10,
-                    courseId = 18817
-                )
-            }
-        }
-
-
     }
 }
