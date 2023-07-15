ba98f63280e7fb6ddfa43c35ce48012af691d6c2
doach <msg_billie@naver.com>
2023-07-15 22:55:37 +0900

    course, lecture domain model 추가

---
+++ app/src/main/java/com/doachgosum/eliceacademyclone/domain/model/CourseModel.kt
@@ -0,0 +1,9 @@
+package com.doachgosum.eliceacademyclone.domain.model
+
+data class CourseModel(
+    val id: Int,
+    val title: String,
+    val imgUrl: String?,
+    val description: String?,
+    val tags: List<String>
+)
---
+++ app/src/main/java/com/doachgosum/eliceacademyclone/domain/model/LectureModel.kt
@@ -0,0 +1,7 @@
+package com.doachgosum.eliceacademyclone.domain.model
+
+data class LectureModel(
+    val id: Int,
+    val title: String,
+    val description: String
+)
